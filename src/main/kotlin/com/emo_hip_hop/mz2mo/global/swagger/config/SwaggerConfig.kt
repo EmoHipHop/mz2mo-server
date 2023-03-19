785476d93f98aebcfc9418f53bcfe6de9ecc5157
devel <develop.raul@gmail.com>
2023-03-19 12:49:02 +0900

    add: SwaggerConfig 작성
    - api 명세 관련 설명 작성

---
+++ src/main/kotlin/com/emo_hip_hop/mz2mo/global/swagger/config/SwaggerConfig.kt
@@ -0,0 +1,26 @@
+    package com.emo_hip_hop.mz2mo.global.swagger.config
+
+    import io.swagger.v3.oas.models.OpenAPI
+    import io.swagger.v3.oas.models.info.Info
+    import org.springdoc.core.GroupedOpenApi
+    import org.springframework.context.annotation.Bean
+    import org.springframework.context.annotation.Configuration
+
+    @Configuration
+    class SwaggerConfig {
+        @Bean
+        fun groupedApi(): GroupedOpenApi {
+            return GroupedOpenApi.builder()
+                .group("V1 API")
+                .pathsToMatch("/api/v1/**")
+                .build()
+        }
+
+        @Bean
+        fun v1Api(): OpenAPI {
+            return Info().title("이호힙합 - mz2mo API V1")
+                .description("이모힙합팀의 mz2mo 서비스 API 입니다!")
+                .version("1.0.0")
+                .let { info -> OpenAPI().info(info) }
+        }
+    }
