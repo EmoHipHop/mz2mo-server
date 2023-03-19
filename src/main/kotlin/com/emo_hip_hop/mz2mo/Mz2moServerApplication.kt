16518bc59e84d66fc1bbf3b79acf805307b4edac
devel <develop.raul@gmail.com>
2023-03-19 11:15:13 +0900

    remove: 기존 CI/CD 파이프라인 시연시 작성했던 코드 및 레거시 파이프라인 제거

--- src/main/kotlin/com/emo_hip_hop/mz2mo/Mz2moServerApplication.kt
+++ src/main/kotlin/com/emo_hip_hop/mz2mo/Mz2moServerApplication.kt
@@ -7,6 +7,5 @@ import org.springframework.boot.runApplication
 class Mz2moServerApplication
 
 fun main(args: Array<String>) {
-    print("🌫️구름 최강 입사자, 킹✨갓\"루키\"의 T.S.스터디!!! 1주일간 기대하고있을게요!!! 🤩😎😀")
     runApplication<Mz2moServerApplication>(*args)
 }
