644fb4af906e6a1e5e6e836c3c3293d6f6a87d25
devel <develop.raul@gmail.com>
2023-03-21 14:28:48 +0900

    fix: 클래스명 오타 수정

--- src/main/kotlin/com/emo_hip_hop/mz2mo/music/application/port/input/AddMusicArticleCommand.kt
+++ src/main/kotlin/com/emo_hip_hop/mz2mo/music/application/port/input/AddMusicArticleCommand.kt
@@ -3,7 +3,7 @@ package com.emo_hip_hop.mz2mo.music.application.port.input
 import com.emo_hip_hop.mz2mo.global.validate.SelfValidating
 import javax.validation.constraints.NotBlank
 
-class AddMusicCommand(
+class AddMusicArticleCommand(
     @field:NotBlank
     val youtubeTrackId: String
-): SelfValidating<AddMusicCommand>()
+): SelfValidating<AddMusicArticleCommand>()
--- src/main/kotlin/com/emo_hip_hop/mz2mo/music/application/service/AddMusicArticleService.kt
+++ src/main/kotlin/com/emo_hip_hop/mz2mo/music/application/service/AddMusicArticleService.kt
@@ -1,7 +1,7 @@
 package com.emo_hip_hop.mz2mo.music.application.service
 
 import com.emo_hip_hop.mz2mo.music.application.port.input.AddMusicArticleUseCase
-import com.emo_hip_hop.mz2mo.music.application.port.input.AddMusicCommand
+import com.emo_hip_hop.mz2mo.music.application.port.input.AddMusicArticleCommand
 import com.emo_hip_hop.mz2mo.music.application.port.output.CreateMusicArticlePort
 import com.emo_hip_hop.mz2mo.music.application.port.output.ExistsMusicArticlePort
 import com.emo_hip_hop.mz2mo.music.domain.MusicArticle
@@ -13,13 +13,13 @@ class AddMusicArticleService(
     private val createMusicArticlePort: CreateMusicArticlePort,
     private val existsMusicArticlePort: ExistsMusicArticlePort
 ): AddMusicArticleUseCase {
-    override fun invoke(command: AddMusicCommand): MusicArticle {
+    override fun invoke(command: AddMusicArticleCommand): MusicArticle {
         checkAlreadyExists(command)
         val musicArticle = MusicArticle.withoutId(command.youtubeTrackId, MusicVotes.empty())
         return createMusicArticlePort.create(musicArticle)
     }
 
-    private fun checkAlreadyExists(command: AddMusicCommand) {
+    private fun checkAlreadyExists(command: AddMusicArticleCommand) {
         val isExists = existsMusicArticlePort.byId(command.youtubeTrackId)
         if (isExists) throw MusicArticleAlreadyExistsException(command.youtubeTrackId)
     }
