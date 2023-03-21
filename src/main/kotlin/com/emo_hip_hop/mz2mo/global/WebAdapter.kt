f3cfbe8b0cf14aa5223a69d17b56108bbb771c5b
devel <develop.raul@gmail.com>
2023-03-21 14:23:36 +0900

    add: Component Annotation 작성 및 적용

---
+++ src/main/kotlin/com/emo_hip_hop/mz2mo/global/WebAdapter.kt
@@ -0,0 +1,18 @@
+package com.emo_hip_hop.mz2mo.global
+
+import org.springframework.core.annotation.AliasFor
+import org.springframework.stereotype.Component
+
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+@Component
+annotation class WebAdapter(
+    /**
+     * The value may indicate a suggestion for a logical component name,
+     * to be turned into a Spring bean in case of an autodetected component.
+     * @return the suggested component name, if any (or empty String otherwise)
+     */
+    @get:AliasFor(annotation = Component::class) val value: String = ""
+)
--- src/main/kotlin/com/emo_hip_hop/mz2mo/music/adaptor/output/persistence/MusicArticlePersistenceAdaptor.kt
+++ src/main/kotlin/com/emo_hip_hop/mz2mo/music/adaptor/output/persistence/MusicArticlePersistenceAdaptor.kt
@@ -1,11 +1,12 @@
 package com.emo_hip_hop.mz2mo.music.adaptor.output.persistence
 
+import com.emo_hip_hop.mz2mo.global.PersistenceAdapter
 import com.emo_hip_hop.mz2mo.music.application.port.output.CreateMusicArticlePort
 import com.emo_hip_hop.mz2mo.music.application.port.output.ExistsMusicArticlePort
 import com.emo_hip_hop.mz2mo.music.domain.MusicArticle
 import org.springframework.stereotype.Component
 
-@Component
+@PersistenceAdapter
 class MusicArticlePersistenceAdaptor(
     private val musicArticleRepository: SpringDataMusicArticleRepository,
     private val musicVoteRepository: SpringDataMusicVoteRepository
