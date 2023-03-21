9a92d6547e2d11ab6a40f3548fe62fe0cf022201
devel <develop.raul@gmail.com>
2023-03-21 14:08:25 +0900

    fix: 도메인 프로퍼티 수정

--- src/main/kotlin/com/emo_hip_hop/mz2mo/music/domain/MusicVote.kt
+++ src/main/kotlin/com/emo_hip_hop/mz2mo/music/domain/MusicVote.kt
@@ -1,9 +1,9 @@
 package com.emo_hip_hop.mz2mo.music.domain
 
 import com.emo_hip_hop.mz2mo.account.domain.AccountId
-import com.emo_hip_hop.mz2mo.emoji.domain.Emoji
+import com.emo_hip_hop.mz2mo.emoji.domain.EmojiId
 
 data class MusicVote(
     val accountId: AccountId,
-    val emoji: Emoji
+    val emojiId: EmojiId
 )
