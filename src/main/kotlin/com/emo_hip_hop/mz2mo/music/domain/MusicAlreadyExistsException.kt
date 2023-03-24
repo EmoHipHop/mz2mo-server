304a99a1cea92c0e79000422867ad53c6b329277
devel <develop.raul@gmail.com>
2023-03-24 17:25:35 +0900

    fix: 예외 작성 로직 리팩토링
    - key/value 를 생성자로 받음으로서 추가 예외 발생상황에대한 로직 고도화 진행

--- src/main/kotlin/com/emo_hip_hop/mz2mo/music/domain/MusicAlreadyExistsException.kt
+++ src/main/kotlin/com/emo_hip_hop/mz2mo/music/domain/MusicAlreadyExistsException.kt
@@ -1,5 +1,5 @@
 package com.emo_hip_hop.mz2mo.music.domain
 
-class MusicAlreadyExistsException(val youtubeId: String) : RuntimeException("music that youtubeId is '$youtubeId' is already exists!") {
+class MusicAlreadyExistsException(key: String, value: String) : RuntimeException("music already exists! - $key: '$value'") {
 
 }
