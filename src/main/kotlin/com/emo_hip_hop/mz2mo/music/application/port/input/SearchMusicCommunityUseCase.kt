06c1d9d0836bbe5c6b231c6cd2ec2338bbad3830
devel <develop.raul@gmail.com>
2023-03-26 17:40:57 +0900

    fix: 음악 커뮤니티 검색 유스케이스 반환형 수정

--- src/main/kotlin/com/emo_hip_hop/mz2mo/music/application/port/input/SearchMusicCommunityUseCase.kt
+++ src/main/kotlin/com/emo_hip_hop/mz2mo/music/application/port/input/SearchMusicCommunityUseCase.kt
@@ -3,5 +3,5 @@ package com.emo_hip_hop.mz2mo.music.application.port.input
 import com.emo_hip_hop.mz2mo.music.domain.MusicCommunity
 
 interface SearchMusicCommunityUseCase {
-    operator fun invoke(query: SearchMusicCommunityQuery): MusicCommunity
+    operator fun invoke(query: SearchMusicCommunityQuery): List<MusicCommunity>
 }
