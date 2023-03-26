07898fd46da9aac8175226d686f24135a3afaafc
devel <develop.raul@gmail.com>
2023-03-26 18:39:05 +0900

    add: 음악 커뮤니티 검색 관련 PersistenceAdapter 작성
    및 이에 필요한 Repository 작성

--- src/main/kotlin/com/emo_hip_hop/mz2mo/music/adapter/output/persistence/SpringDataCustomMusicCommunityRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.emo_hip_hop.mz2mo.music.adapter.output.persistence
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface SpringDataCustomMusicCommunityRepository: JpaRepository<MusicCommunitiesJpaEntity, String>, CustomMusicCommunityRepository {
-    fun findByMusicId(musicId: String): MusicCommunitiesJpaEntity?
-}
