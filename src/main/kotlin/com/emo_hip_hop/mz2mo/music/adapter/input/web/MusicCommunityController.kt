9e3b1d12a1016b0f538075604b3b0af670554464
devel <develop.raul@gmail.com>
2023-03-26 18:54:37 +0900

    add: MusicArticleController에 커뮤니티 검색 관련 endpoint 추가 및 이에 필요한 컴포넌트 작성

--- src/main/kotlin/com/emo_hip_hop/mz2mo/music/adapter/input/web/MusicCommunityController.kt
+++ src/main/kotlin/com/emo_hip_hop/mz2mo/music/adapter/input/web/MusicCommunityController.kt
@@ -1,9 +1,9 @@
 package com.emo_hip_hop.mz2mo.music.adapter.input.web
 
 import com.emo_hip_hop.mz2mo.global.WebAdapter
+import com.emo_hip_hop.mz2mo.music.domain.MusicId
 import com.emo_hip_hop.mz2mo.global.common.domain.Pageable
 import com.emo_hip_hop.mz2mo.music.application.port.input.*
-import com.emo_hip_hop.mz2mo.music.domain.MusicId
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
