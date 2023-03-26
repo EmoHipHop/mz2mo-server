0a15c2dfa98b03dff04c459c777feb567211fb18
devel <develop.raul@gmail.com>
2023-03-27 08:10:30 +0900

    fix: MusicCommunityPartialNotFoundException 네이밍 및 메세지 수정

--- src/main/kotlin/com/emo_hip_hop/mz2mo/music/adapter/input/web/MusicCommunityControllerAdvice.kt
+++ src/main/kotlin/com/emo_hip_hop/mz2mo/music/adapter/input/web/MusicCommunityControllerAdvice.kt
@@ -1,7 +1,7 @@
 package com.emo_hip_hop.mz2mo.music.adapter.input.web
 
 import com.emo_hip_hop.mz2mo.music.domain.MusicAlreadyExistsException
-import com.emo_hip_hop.mz2mo.music.domain.MusicCommunityPartialNotFoundException
+import com.emo_hip_hop.mz2mo.music.domain.MusicCommunityOrPartialNotFoundException
 import com.emo_hip_hop.mz2mo.music.domain.MusicOutOfSyncException
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -17,8 +17,8 @@ class MusicCommunityControllerAdvice {
     fun handle(e: MusicOutOfSyncException): ResponseEntity<String> {
         return ResponseEntity.internalServerError().body(e.message)
     }
-    @ExceptionHandler(MusicCommunityPartialNotFoundException::class)
-    fun handle(e: MusicCommunityPartialNotFoundException): ResponseEntity<String> {
+    @ExceptionHandler(MusicCommunityOrPartialNotFoundException::class)
+    fun handle(e: MusicCommunityOrPartialNotFoundException): ResponseEntity<String> {
         return ResponseEntity.badRequest().body(e.message)
     }
 }
--- src/main/kotlin/com/emo_hip_hop/mz2mo/music/domain/MusicCommunityPartialNotFoundException.kt
+++
@@ -1,3 +0,0 @@
-package com.emo_hip_hop.mz2mo.music.domain
-
-class MusicCommunityPartialNotFoundException(key: String, value: String) : RuntimeException("music community partial not found! - $key: '$value'")
