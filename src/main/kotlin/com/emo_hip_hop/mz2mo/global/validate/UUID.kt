682dfd83cb67e89d16ffed3e775a982013216da7
devel <develop.raul@gmail.com>
2023-03-27 08:02:42 +0900

    fix: uuid 관련 스팩을 validate 도메인으로 이전

--- src/main/kotlin/com/emo_hip_hop/mz2mo/global/validate/UUID.kt
+++ src/main/kotlin/com/emo_hip_hop/mz2mo/global/validate/UUID.kt
@@ -1,4 +1,4 @@
-package com.emo_hip_hop.mz2mo.global.swagger
+package com.emo_hip_hop.mz2mo.global.validate
 
 const val UUID_PATTERN = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}\$"
 const val UUID_EXAMPLE = "6c8db601-ac00-4a31-a5ec-e728a8225f85"
