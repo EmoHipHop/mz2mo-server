989244f0c32e7694e4fb794a46b57c16d3a48aee
devel <develop.raul@gmail.com>
2023-03-30 00:44:30 +0900

    add: 음악 투표 삭제 유스케이스 및 이에 필요한 컴포넌트 작성

---
+++ src/main/kotlin/com/emo_hip_hop/mz2mo/music/application/service/RemoveMusicVoteService.kt
@@ -0,0 +1,38 @@
+package com.emo_hip_hop.mz2mo.music.application.service
+
+import com.emo_hip_hop.mz2mo.global.UseCase
+import com.emo_hip_hop.mz2mo.music.application.port.input.RemoveMusicVoteCommand
+import com.emo_hip_hop.mz2mo.music.application.port.input.RemoveMusicVoteUseCase
+import com.emo_hip_hop.mz2mo.music.application.port.output.QueryMusicCommunityPort
+import com.emo_hip_hop.mz2mo.music.application.port.output.UpdateMusicCommunityPort
+import com.emo_hip_hop.mz2mo.music.domain.MusicCommunity
+import com.emo_hip_hop.mz2mo.music.domain.exception.MusicCommunityOrPartialNotFoundException
+import com.emo_hip_hop.mz2mo.music.domain.exception.VoteNotFoundException
+
+@UseCase
+class RemoveMusicVoteService(
+    private val queryMusicCommunityPort: QueryMusicCommunityPort,
+    private val updateMusicCommunityPort: UpdateMusicCommunityPort
+) : RemoveMusicVoteUseCase {
+    override fun invoke(command: RemoveMusicVoteCommand): MusicCommunity {
+        val musicId = command.musicId
+        val musicCommunity = queryMusicCommunityPort.findByMusicId(musicId)
+            ?: throw MusicCommunityOrPartialNotFoundException("musicId", musicId.id)
+
+        checkCanRemoveVote(musicCommunity, command)
+
+        val voteToRemove = musicCommunity.votes.first {
+            it.accountId.id == command.accountId.id &&
+                it.emojiId.id == command.emojiId.id
+        }
+        val musicCommunityToUpdate = musicCommunity.removeVote(voteToRemove)
+        return updateMusicCommunityPort.update(musicCommunityToUpdate)
+    }
+
+    private fun checkCanRemoveVote(musicCommunity: MusicCommunity, command: RemoveMusicVoteCommand) {
+        val musicId = command.musicId
+
+        val currentVoteCount = musicCommunity.votes.size
+        if (currentVoteCount <= 0) throw VoteNotFoundException(musicId, command.accountId, command.emojiId)
+    }
+}
---
+++ src/main/kotlin/com/emo_hip_hop/mz2mo/music/domain/exception/VoteNotFoundException.kt
@@ -0,0 +1,11 @@
+package com.emo_hip_hop.mz2mo.music.domain.exception
+
+import com.emo_hip_hop.mz2mo.account.domain.AccountId
+import com.emo_hip_hop.mz2mo.emoji.domain.EmojiId
+import com.emo_hip_hop.mz2mo.music.domain.MusicId
+
+class VoteNotFoundException(
+    musicId: MusicId,
+    accountId: AccountId,
+    emojiId: EmojiId
+) : RuntimeException("vote not found! - $accountId: '${accountId.id}', $emojiId: '${emojiId.id}', $musicId: '${musicId.id}")
