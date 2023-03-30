c0d0274819a31b6d9bcb4cceaf7110a0109076bb
devel <develop.raul@gmail.com>
2023-03-31 04:52:23 +0900

    fix: MusicControllerAdvice에 ErrorResponse 스팩 적용

--- src/main/kotlin/com/emo_hip_hop/mz2mo/global/error/domain/ErrorCode.kt
+++ src/main/kotlin/com/emo_hip_hop/mz2mo/global/error/domain/ErrorCode.kt
@@ -6,7 +6,7 @@ enum class ErrorCode(
     val index: Int
 ) {
     ALREADY_VOTE(DomainCode.MUSIC_VOTE, CauseOriginCode.USER, 1),
-    EXCEED_MAXIMUM_VOTE(DomainCode.MUSIC_VOTE, CauseOriginCode.USER, 2),
+    EXCEED_MAXIMUM_VOTE_PER_USER(DomainCode.MUSIC_VOTE, CauseOriginCode.USER, 2),
     EMPTY_MUSIC_ID(DomainCode.MUSIC_VOTE, CauseOriginCode.CLIENT, 3),
     MUSIC_OUT_OF_SYNC(DomainCode.MUSIC, CauseOriginCode.SERVER, 4),
     MUSIC_COMMUNITY_OR_PARTIAL_NOT_FOUND(DomainCode.MUSIC, CauseOriginCode.SERVER, 5),
--- src/main/kotlin/com/emo_hip_hop/mz2mo/music/adapter/input/web/MusicControllerAdvice.kt
+++ src/main/kotlin/com/emo_hip_hop/mz2mo/music/adapter/input/web/MusicControllerAdvice.kt
@@ -1,5 +1,7 @@
 package com.emo_hip_hop.mz2mo.music.adapter.input.web
 
+import com.emo_hip_hop.mz2mo.global.error.adapter.input.web.ErrorResponse
+import com.emo_hip_hop.mz2mo.global.error.domain.ErrorCode
 import com.emo_hip_hop.mz2mo.music.domain.*
 import com.emo_hip_hop.mz2mo.music.domain.exception.*
 import org.springframework.http.HttpStatus
@@ -10,35 +12,43 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 @RestControllerAdvice(basePackageClasses = [MusicCommunityController::class, MusicVoteController::class])
 class MusicControllerAdvice {
     @ExceptionHandler(MusicAlreadyExistsException::class)
-    fun handle(e: MusicAlreadyExistsException): ResponseEntity<String> {
-        return ResponseEntity.badRequest().body(e.message)
+    fun handle(e: MusicAlreadyExistsException): ResponseEntity<ErrorResponse> {
+        val response = ErrorResponse.of(ErrorCode.MUSIC_ALREADY_EXISTS, e.message)
+        return ResponseEntity.badRequest().body(response)
     }
     @ExceptionHandler(MusicOutOfSyncException::class)
-    fun handle(e: MusicOutOfSyncException): ResponseEntity<String> {
-        return ResponseEntity.internalServerError().body(e.message)
+    fun handle(e: MusicOutOfSyncException): ResponseEntity<ErrorResponse> {
+        val response = ErrorResponse.of(ErrorCode.MUSIC_OUT_OF_SYNC, e.message)
+        return ResponseEntity.internalServerError().body(response)
     }
     @ExceptionHandler(AlreadyVoteException::class)
-    fun handle(e: AlreadyVoteException): ResponseEntity<String> {
-        return ResponseEntity.badRequest().body(e.message)
+    fun handle(e: AlreadyVoteException): ResponseEntity<ErrorResponse> {
+        val response = ErrorResponse.of(ErrorCode.ALREADY_VOTE, e.message)
+        return ResponseEntity.badRequest().body(response)
     }
     @ExceptionHandler(EmptyMusicIdException::class)
-    fun handle(e: EmptyMusicIdException): ResponseEntity<String> {
-        return ResponseEntity.badRequest().body(e.message)
+    fun handle(e: EmptyMusicIdException): ResponseEntity<ErrorResponse> {
+        val response = ErrorResponse.of(ErrorCode.EMPTY_MUSIC_ID, e.message)
+        return ResponseEntity.badRequest().body(response)
     }
     @ExceptionHandler(ExceedMaximumVotesPerUserException::class)
-    fun handle(e: ExceedMaximumVotesPerUserException): ResponseEntity<String> {
-        return ResponseEntity.badRequest().body(e.message)
+    fun handle(e: ExceedMaximumVotesPerUserException): ResponseEntity<ErrorResponse> {
+        val response = ErrorResponse.of(ErrorCode.EXCEED_MAXIMUM_VOTE_PER_USER, e.message)
+        return ResponseEntity.badRequest().body(response)
     }
     @ExceptionHandler(MusicCommunityOrPartialNotFoundException::class)
-    fun handle(e: MusicCommunityOrPartialNotFoundException): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.message)
+    fun handle(e: MusicCommunityOrPartialNotFoundException): ResponseEntity<ErrorResponse> {
+        val response = ErrorResponse.of(ErrorCode.MUSIC_COMMUNITY_OR_PARTIAL_NOT_FOUND, e.message)
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response)
     }
     @ExceptionHandler(MusicNotFoundException::class)
-    fun handle(e: MusicNotFoundException): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.message)
+    fun handle(e: MusicNotFoundException): ResponseEntity<ErrorResponse> {
+        val response = ErrorResponse.of(ErrorCode.MUSIC_NOT_FOUND, e.message)
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response)
     }
     @ExceptionHandler(MusicVoteNotFoundException::class)
-    fun handle(e: MusicVoteNotFoundException): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.message)
+    fun handle(e: MusicVoteNotFoundException): ResponseEntity<ErrorResponse> {
+        val response = ErrorResponse.of(ErrorCode.MUSIC_VOTE_NOT_FOUND, e.message)
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response)
     }
 }
