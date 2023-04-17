c7e6183b2397f72c79e01b8438f767d992f8c5a5
devel <develop.raul@gmail.com>
2023-04-18 07:47:26 +0900

    fix: cors configuration

--- src/main/kotlin/com/emo_hip_hop/mz2mo/global/WebConfig.kt
+++ src/main/kotlin/com/emo_hip_hop/mz2mo/global/WebConfig.kt
@@ -1,15 +1,20 @@
 package com.emo_hip_hop.mz2mo.global
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig : WebMvcConfigurer {
+class WebConfig(
+    @Value("\${cors.allowedOrigins}")
+    private val allowedOrigins: List<String>
+) : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("*")
+            .allowedOriginPatterns(*allowedOrigins.toTypedArray())
             .allowedMethods("*")
+            .allowedHeaders("*")
             .allowCredentials(true)
             .maxAge(3600)
     }
